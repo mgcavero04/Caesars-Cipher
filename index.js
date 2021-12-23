f7230b7cf706e7bcee9b917207583fd4525fb89b
Martha Garcia <mgcavero04@hotmail.com>
2021-12-22 23:20:04 -0500

    Update index.js
    added comments

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function getletter(l){
   var alpha=["A", "B", "C", "D", "E", "F","G", "H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var newreplace="";
-  var foundIndex=alpha.indexOf(l);
-  if(foundIndex+13>=alpha.length){
-    foundIndex=foundIndex+13-alpha.length;
+  var foundIndex=alpha.indexOf(l);//get the index number of the letter to replace
+  if(foundIndex+13>=alpha.length){//if the letter +13 is not in the alpha array
+    foundIndex=foundIndex+13-alpha.length;//subtract from length to get new index from beginning of alpha array
     newreplace=alpha[foundIndex];
     }
   else{
@@ -15,12 +15,12 @@ function rot13(str) {
   var regex = ["A", "B", "C", "D", "E", "F","G", "H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var toreplace="";
   for(let i=0; i<str.length; i++){
-    var theChar = str.charAt(i);
-     if(regex.indexOf(theChar) != -1){
-        toreplace=toreplace+getletter(str[i]);
+    var theChar = str.charAt(i);//get each letter in the str
+     if(regex.indexOf(theChar) != -1){//if the letter from str is in the regex array
+        toreplace=toreplace+getletter(str[i]);//calls getletter() function with new letter and concatenate
       }
     else{
-        toreplace=toreplace+theChar;
+        toreplace=toreplace+theChar;//else do not replace, just concatenate
       }
     }
     return toreplace;
